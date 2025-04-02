24d9ac3f9d23ca8009c5408f5b72e331481367e9
Ravi Shankar <ravisingh18879@gmail.com>
2025-04-02 21:43:53 +0530

    upload

--- src/utils/imageUtils.js
+++ src/utils/imageUtils.js
@@ -1,9 +1,21 @@
-export const getRandomAvatar = () => {
-    // Example avatar URLs (replace with your own)
+// src/utils/imageUtils.js
+
+// For user avatars (existing function)
+export const getRandomAvatar = (userId) => {
     const avatars = [
     "https://i.pravatar.cc/150?img=1",
     "https://i.pravatar.cc/150?img=2",
-    "https://i.pravatar.cc/150?img=3",
+    "https://i.pravatar.cc/150?img=3"
     ];
-    return avatars[Math.floor(Math.random() * avatars.length)];
+    return avatars[userId % avatars.length];
+};
+
+  // Add this new function for post images
+export const getRandomPostImage = (postId) => {
+    const postImages = [
+    "https://picsum.photos/500/300?nature",
+    "https://picsum.photos/500/300?tech",
+    "https://picsum.photos/500/300?people"
+    ];
+    return postImages[postId % postImages.length];
 };
